213dfd6854cb5b3649e4de6526da5c9a2013c51b
Markus Felten <Markus.Felten@gmx.de>
2015-03-28 13:08:48 +0100

    dummy impls for getEndpoint(),getEndpointConfiguration() and getServiceConfiguration()

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ function createService(manager, serviceId, serviceConfig) {
 }
 */
 
+
+let serviceDefinitions = {};
+
 exports.manager = {
 
+	declareServices: function (sDefs) {
+		serviceDefinitions = sDefs;
+	},
 
-  getServiceConfiguration: function (serviceId) {},
-  getEndpointConfiguration: function (serviceId, endpointId) {},
+	getServiceConfiguration: function (serviceId) {
+		return serviceDefinitions[serviceId];
+	},
 
+	getEndpointConfiguration: function (serviceId, endpointId) {
+		return serviceDefinitions[serviceId].endpoints[endpointId];
+	},
 
-/*
-   service interface used by a service
-*/
-  getEndpoint: function (serviceId, endpointId) {}
+	/*
+	   service interface used by a service
+	*/
+	getEndpoint: function (serviceId, endpointId) {
+		return serviceDefinitions[serviceId].endpoints[endpointId];
+	}
 
 };
