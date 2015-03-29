aaf95b484342fd62e36d3653f3a509fef2428488
Markus Felten <Markus.Felten@gmx.de>
2015-03-29 12:44:16 +0200

    some tests added

--- index.js
+++ index.js
@@ -12,35 +12,56 @@ function createService(manager, serviceId, serviceConfig) {
 exports.manager = function () {
 
   let serviceDefinitions = {};
-  let instantiatedServices = {};
+
+  function getService(serviceId) {
+    return serviceDefinitions[serviceId];
+  }
+
+  function getServiceConfiguration(serviceId) {
+    return getService(serviceId).config;
+  }
 
   function getEndpointConfiguration(serviceId, endpointId) {
-    return serviceDefinitions[serviceId].endpoints[endpointId];
+    const service = getService(serviceId);
+    if (service) {
+      return service.endpoints[endpointId];
+    }
+    return undefined;
   }
 
   let manager = {
+    declareServices: function (sDefs) {
+      for (let sid in sDefs) {
+        const sd = sDefs[sid];
+        sd.id = sid;
+        if (sd.config === undefined) {
+          sd.config = {};
+        }
+        if (sd.endpoints === undefined) {
+          sd.endpoints = {};
+        }
+
+        serviceDefinitions[sid] = sd;
+      }
+    },
+
     instantiateService: function (serviceId, factory) {
-      let service = factory(manager, serviceId, {});
-      instantiatedServices[serviceId] = service;
+      let sd = getService(serviceId);
+      let service = factory(manager, serviceId, sd.config);
+      sd.instance = service;
       return service;
     },
 
-    declareServices: function (sDefs) {
-      serviceDefinitions = sDefs;
-    },
-
-    getServiceConfiguration: function (serviceId) {
-      return serviceDefinitions[serviceId].config;
-    },
-
-    getEndpointConfiguration: getEndpointConfiguration,
+    getServiceConfiguration: getServiceConfiguration,
+    getService: getService,
 
     /*
 		   service interface used by a service
 		*/
-    getEndpoint: function (serviceId, endpointId) {
+    getServiceEndpoint: function (serviceId, endpointId) {
       return getEndpointConfiguration(serviceId, endpointId);
-    }
+    },
+
   };
 
   return manager;
--- test/index.js
+++ test/index.js
@@ -8,20 +8,32 @@ var assert = require('assert');
 describe('service declaration', function () {
 	let myManager = manager();
 	myManager.declareServices({
-		'service1': {
-			'config': {
+		"service1": {
+			"module": "dummy",
+			"config": {
 				"key1": "value1"
 			},
-			'endpoints': {
-				'in1': 'stdin',
-				'out1': function (info, stream, cb) {}
+			"endpoints": {
+				"in1": "stdin",
+				"out1": function (info, stream, cb) {}
 			}
 		}
 	});
-	it('declared services should be present', function () {
-		let cfg = myManager.getServiceConfiguration('service1');
-		assert(cfg.key1 === "value1");
+	it('service should be present', function () {
+		let service = myManager.getService('service1');
+		assert(service.id === "service1");
 	});
+
+	it('service config should be present', function () {
+		let config = myManager.getServiceConfiguration('service1');
+		assert(config.key1 === "value1");
+	});
+
+	it('service endpoint should be present', function () {
+		let out1 = myManager.getServiceEndpoint('service1', 'out1');
+		assert(out1);
+	});
+
 });
 
 
