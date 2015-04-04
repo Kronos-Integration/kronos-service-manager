b3f4a2dfaaad743066fd8d9228c0b848aa7d0c22
Markus Felten <Markus.Felten@gmx.de>
2015-04-04 21:50:23 +0200

    registering of steps and endpoints streamlined

--- test/manager.js
+++ test/manager.js
@@ -2,35 +2,35 @@
 
 "use strict";
 
-let manager = require('../index.js').manager;
+let manager = require('../lib/manager.js').manager;
 var assert = require('assert');
 
 describe('service declaration', function () {
-	let myManager = manager();
-	myManager.declareFlow({
-		"name": "myFlow",
-		"steps": {
-			"s1": {
-				"type": "copy",
-				"config": {
-					"key1": "value1"
-				},
-				"endpoints": {
-					"in": "stdin",
-					"out": function* () {
-						do {
-							let request =
-								yield;
-						} while (true);
-					}
-				}
-			}
-		}
-	});
-	it('flow should be present', function () {
-		let flow = myManager.getFlow('myFlow');
-		assert(flow.name === "myFlow");
-	});
+  let myManager = manager();
+  myManager.declareFlow({
+    "name": "myFlow",
+    "steps": {
+      "s1": {
+        "type": "copy",
+        "config": {
+          "key1": "value1"
+        },
+        "endpoints": {
+          "in": "stdin",
+          "out": function* () {
+            do {
+              let request =
+                yield;
+            } while (true);
+          }
+        }
+      }
+    }
+  });
+  it('flow should be present', function () {
+    let flow = myManager.getFlow('myFlow');
+    assert(flow.name === "myFlow");
+  });
 });
 
 
--- test/stepImplementation.js
+++ test/stepImplementation.js
@@ -2,36 +2,36 @@
 
 "use strict";
 
-let stepImplementation = require('../lib/stepImplementation');
+let stepImpl = require('../lib/stepImplementation');
 
 var assert = require('assert');
 
 describe('step implementation', function () {
 
-	const si = stepImplementation.stepImplementations.copy;
+  const si = stepImpl.implementations.copy;
 
-	it('buildin copy step present', function () {
-		assert(si.name === "copy");
-	});
+  it('buildin copy step present', function () {
+    assert(si.name === "copy");
+  });
 
-	it('endpoint definition - in', function () {
-		assert(si.endpoints.in.name === "in");
-	});
+  it('endpoint definition - in', function () {
+    assert(si.endpoints.in.name === "in");
+  });
 
-	it('endpoint definition defaults - in', function () {
-		assert(si.endpoints.in.mandatory);
-		assert(si.endpoints.in.uti === 'public.data');
-		assert(si.endpoints.in.direction === 'in');
-	});
+  it('endpoint definition defaults - in', function () {
+    assert(si.endpoints.in.mandatory);
+    assert(si.endpoints.in.uti === 'public.data');
+    assert(si.endpoints.in.direction === 'in');
+  });
 
-	it('endpoint definition - out', function () {
-		assert(si.endpoints.out.name === "out");
-		assert(si.endpoints.out.direction === 'out');
-	});
+  it('endpoint definition - out', function () {
+    assert(si.endpoints.out.name === "out");
+    assert(si.endpoints.out.direction === 'out');
+  });
 
-	it('endpoint definition defaults - out', function () {
-		assert(si.endpoints.out.uti === 'public.data');
-		assert(si.endpoints.out.mandatory);
-	});
+  it('endpoint definition defaults - out', function () {
+    assert(si.endpoints.out.uti === 'public.data');
+    assert(si.endpoints.out.mandatory);
+  });
 
 });
