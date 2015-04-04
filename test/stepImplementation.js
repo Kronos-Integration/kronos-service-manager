de07fe4ac07a10a63c2d820ebfcdb6cd71e05ed1
Markus Felten <Markus.Felten@gmx.de>
2015-04-04 13:43:07 +0200

    make more use of stepImplementation

--- test/stepImplementation.js
+++ test/stepImplementation.js
@@ -8,10 +8,17 @@ var assert = require('assert');
 
 describe('step implementation', function () {
 
-  it('buildin copy step present', function () {
-    const si = stepImplementation.stepImplementations.copy;
-    assert(si.name === "copy");
-    assert(si.endpoints.in.name === "in");
-  });
+	const si = stepImplementation.stepImplementations.copy;
+
+	it('buildin copy step present', function () {
+		assert(si.name === "copy");
+	});
+
+	it('endpoint definition present', function () {
+		assert(si.endpoints.in.name === "in");
+		assert(si.endpoints.in.mandatory);
+		assert(si.endpoints.in.uti === 'public.data');
+		assert(si.endpoints.in.direction === 'in');
+	});
 
 });
