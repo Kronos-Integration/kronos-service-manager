1355619e54136ba046061db1d5c3df4809f90567
Markus Felten <Markus.Felten@gmx.de>
2015-04-04 14:05:14 +0200

    handle endpoint defaults (uti=public.data,direction=in,mandatory=true) well

--- test/stepImplementation.js
+++ test/stepImplementation.js
@@ -14,11 +14,24 @@ describe('step implementation', function () {
 		assert(si.name === "copy");
 	});
 
-	it('endpoint definition present', function () {
+	it('endpoint definition - in', function () {
 		assert(si.endpoints.in.name === "in");
+	});
+
+	it('endpoint definition defaults - in', function () {
 		assert(si.endpoints.in.mandatory);
 		assert(si.endpoints.in.uti === 'public.data');
 		assert(si.endpoints.in.direction === 'in');
+	});
+
+	it('endpoint definition - out', function () {
+		assert(si.endpoints.out.name === "out");
+		assert(si.endpoints.out.direction === 'out');
+	});
+
+	it('endpoint definition defaults - out', function () {
+		assert(si.endpoints.out.uti === 'public.data');
+		assert(si.endpoints.out.mandatory);
 	});
 
 });
