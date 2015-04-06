248838476b56cad56c53bd77914c62fbcf2ceb0e
Markus Felten <Markus.Felten@gmx.de>
2015-04-06 22:28:52 +0200

    flow initializing - wip

--- test/flow.js
+++ test/flow.js
@@ -12,53 +12,53 @@ let endpointImpl = require('../lib/endpointImplementation');
 var assert = require('assert');
 
 describe('service declaration', function () {
-  let myFlow = flow.create({
-    "name": "myFlow",
-    "steps": {
-      "s1": {
-        "type": "copy",
-        "config": {
-          "port": 77
-        },
-        "endpoints": {
-          "in": "stdin",
-          "out": "step:s2/in"
-        }
-      },
-      "s2": {
-        "type": "copy",
-        "endpoints": {
-          "out": "file:/tmp/sample1.txt"
-        }
-      }
-    }
-  });
+	let myFlow = flow.create({
+		"name": "myFlow",
+		"steps": {
+			"s1": {
+				"type": "copy",
+				"config": {
+					"port": 77
+				},
+				"endpoints": {
+					"in": "stdin",
+					"out": "step:s2/in"
+				}
+			},
+			"s2": {
+				"type": "copy",
+				"endpoints": {
+					"out": "file:/tmp/sample1.txt"
+				}
+			}
+		}
+	});
 
-  it('flow name shoud be present', function () {
-    assert(myFlow.name === "myFlow");
-  });
+	it('flow name shoud be present', function () {
+		assert(myFlow.name === "myFlow");
+	});
 
-  it('steps shoud be present', function () {
-    assert(myFlow.steps.s1.name === "s1");
-  });
+	it('steps shoud be present', function () {
+		assert(myFlow.steps.s1.name === "s1");
+	});
 
-  it('steps shoud have a implementation', function () {
-    assert(myFlow.steps.s1.implementation.name === "copy");
-  });
+	it('steps shoud have a implementation', function () {
+		assert(myFlow.steps.s1.implementation.name === "copy");
+	});
 
-  it('steps config shoud be present', function () {
-    assert(myFlow.steps.s1.config.port === 77);
-  });
+	it('steps config shoud be present', function () {
+		assert(myFlow.steps.s1.config.port === 77);
+	});
 
-  it('endpoints shoud be present', function () {
-    assert(myFlow.steps.s1.endpoints.out.name === "out");
-  });
+	it('endpoints shoud be present', function () {
+		assert(myFlow.steps.s1.endpoints.out.name === "out");
+	});
 
-  it('endpoints counterparts shoud be linked', function () {
-    assert(myFlow.steps.s1.endpoints.out.counterpart.name === 'in');
-    assert(myFlow.steps.s2.endpoints.in.counterpart.name === 'out');
+	it('endpoints counterparts shoud be linked', function () {
+		assert(myFlow.steps.s1.endpoints.out.counterpart.name === 'in');
+		assert(myFlow.steps.s2.endpoints.in.counterpart.name === 'out');
 
-    /*	assert(myFlow.steps.s1.endpoints.out.counterpart === myFlow.steps.s2
+		/*	assert(myFlow.steps.s1.endpoints.out.counterpart === myFlow.steps.s2
 			.endpoints.in);
     console.log("myFlow.steps.s2.endpoints.in.counterpart : " + myFlow.steps
       .s2.endpoints.in.counterpart);
@@ -67,6 +67,11 @@ describe('service declaration', function () {
     assert(myFlow.steps.s2.endpoints.in.counterpart === myFlow.steps.s1
       .endpoints.out);
 			*/
-  });
+	});
+
+	it('can be initialized', function () {
+		myFlow.initialize();
+		assert(myFlow);
+	});
 
 });
