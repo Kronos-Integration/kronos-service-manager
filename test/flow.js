4c59e12b0836c68242748652bb2c089b4e488264
Markus Felten <Markus.Felten@gmx.de>
2015-04-07 10:39:36 +0200

    testing invalid flow definitions -> progressReport

--- test/flow.js
+++ test/flow.js
@@ -3,6 +3,7 @@
 "use strict";
 
 let flow = require('../lib/flow');
+let progressReporter = require('../lib/progressReporter');
 
 /*
 let stepImpl = require('../lib/stepImplementation');
@@ -11,7 +12,41 @@ let endpointImpl = require('../lib/endpointImplementation');
 
 var assert = require('assert');
 
-describe('service declaration', function () {
+describe('flow declaration error handling', function () {
+
+  let progressEntries = [];
+
+  let myFlow = flow.create({
+    "name": "myFlow",
+    "steps": {
+      "s1": {
+        "type": "copy2",
+        "endpoints": {
+          "in": "stdin",
+          "out": "stdout"
+        }
+      }
+    }
+  }, progressReporter.defaultProgressReporter(function (entry) {
+    progressEntries.push(entry);
+  }));
+
+  it('progress entries should be filled with error', function () {
+    assert(progressEntries.length !== 0);
+    const pe = progressEntries[0];
+    assert(pe.severity === 'error');
+  });
+
+  it('error entry should have scope', function () {
+    const pe = progressEntries[0];
+    assert(pe.scope[0].name === 'flow');
+    assert(pe.scope[0].properties.flow === 'myFlow');
+    assert(pe.scope[1].name === 'step');
+    assert(pe.scope[1].properties.step === 's1');
+  });
+});
+
+describe('flow declaration', function () {
   let myFlow = flow.create({
     "name": "myFlow",
     "steps": {
