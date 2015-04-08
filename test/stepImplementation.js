79f9e5b6dc7b71be8a108d118044e5dda9e38dd0
Markus Felten <Markus.Felten@gmx.de>
2015-04-08 07:03:27 +0200

    added: attributes (contentInfo)

--- test/stepImplementation.js
+++ test/stepImplementation.js
@@ -8,30 +8,31 @@ var assert = require('assert');
 
 describe('step implementation', function () {
 
-  const si = stepImpl.implementations.copy;
+	const si = stepImpl.implementations.copy;
 
-  it('buildin copy step present', function () {
-    assert(si.name === "copy");
-  });
+	it('buildin copy step present', function () {
+		assert(si.name === "copy");
+	});
 
-  it('endpoint definition - in', function () {
-    assert(si.endpoints.in.name === "in");
-  });
+	it('endpoint definition - in', function () {
+		assert(si.endpoints.in.name === "in");
+	});
 
-  it('endpoint definition defaults - in', function () {
-    assert(si.endpoints.in.mandatory);
-    assert(si.endpoints.in.uti === 'public.data');
-    assert(si.endpoints.in.direction === 'in');
-  });
+	it('endpoint definition defaults - in', function () {
+		assert(si.endpoints.in.mandatory);
+		assert(si.endpoints.in.uti === 'public.data');
+		assert(si.endpoints.in.direction === 'in');
+	});
 
-  it('endpoint definition - out', function () {
-    assert(si.endpoints.out.name === "out");
-    assert(si.endpoints.out.direction === 'out');
-  });
+	it('endpoint definition - out', function () {
+		assert(si.endpoints.out.name === "out");
+		assert(si.endpoints.out.direction === 'out');
+	});
 
-  it('endpoint definition defaults - out', function () {
-    assert(si.endpoints.out.uti === 'public.data');
-    assert(si.endpoints.out.mandatory);
-  });
+	it('endpoint definition defaults - out', function () {
+		assert(si.endpoints.out.uti === 'public.data');
+		assert(si.endpoints.out.mandatory);
+		assert(si.endpoints.out.contentInfo);
+	});
 
 });
