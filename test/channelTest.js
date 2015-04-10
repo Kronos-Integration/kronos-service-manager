e142bdc0c9c806085c398dba9eb9d957349c3dda
Markus Felten <Markus.Felten@gmx.de>
2015-04-10 21:37:25 +0200

    WIP

--- test/channelTest.js
+++ test/channelTest.js
@@ -9,13 +9,51 @@ const assert = require('assert');
 
 describe('channel creation', function () {
 	const es = endpointImpls.createEndpointsFromDefinition({
-		"src": {},
-		"dst": {}
+		"input": {
+			"implementation": function () {
+				return function* () {
+					do {
+						const request =
+							yield {
+								info: {
+									name: "input"
+								},
+								stream: "a stream"
+							};
+					}
+					while (true);
+				};
+			}
+		},
+		"output": {
+			"implementation": function () {
+				return function* () {
+					do {
+						const request =
+							yield;
+						console.log(`output got: ${JSON.stringify(request)}`);
+					}
+					while (true);
+				};
+			}
+		}
 	});
 
-	const ecs = channel.create(es.src, es.dst);
+	const ecs = channel.create(es.output, es.input);
 
 	it('endpoints created', function () {
 		assert(ecs.length === 2);
 	});
+
+
+	it('input connected', function () {
+
+		const input = ecs[0].implementation();
+
+		const request = input.next();
+		console.log(`got: ${JSON.stringify(request)}`);
+
+		assert(request);
+	});
+
 });
