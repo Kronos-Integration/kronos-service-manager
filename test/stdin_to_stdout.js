7c5964cc807373972a630fe981296732fa577702
Markus Felten <Markus.Felten@gmx.de>
2015-04-12 21:49:30 +0200

    counterpart endpoind do not need to be declared in flow

--- test/stdin_to_stdout.js
+++ test/stdin_to_stdout.js
@@ -12,37 +12,34 @@ iojs test/stdin_to_stdout.js <test/stdin_to_stdout.js
 const flow = require('../lib/flow');
 
 const myFlow = flow.create({
-  "name": "myFlow",
-  "steps": {
-    "s1": {
-      "type": "copy",
-      "endpoints": {
-        "in": "stdin",
-        "out": "step:s2/in"
-      }
-    },
-    "s2": {
-      "type": "copy",
-      "endpoints": {
-        "in": "step:s1/out",
-        "out": "step:s3/in"
-      }
-    },
-    "s3": {
-      "type": "copy",
-      "endpoints": {
-        "in": "step:s2/out",
-        "out": "step:s4/in"
-      }
-    },
-    "s4": {
-      "type": "copy",
-      "endpoints": {
-        "in": "step:s3/out",
-        "out": "stdout"
-      }
-    }
-  }
+	"name": "myFlow",
+	"steps": {
+		"s1": {
+			"type": "copy",
+			"endpoints": {
+				"in": "stdin",
+				"out": "step:s2/in"
+			}
+		},
+		"s2": {
+			"type": "copy",
+			"endpoints": {
+				"out": "step:s3/in"
+			}
+		},
+		"s3": {
+			"type": "copy",
+			"endpoints": {
+				"out": "step:s4/in"
+			}
+		},
+		"s4": {
+			"type": "copy",
+			"endpoints": {
+				"out": "stdout"
+			}
+		}
+	}
 });
 
 myFlow.initialize();
