aa85f28c0aa020c24cc07e90c0ab2b5fdb98e338
Markus Felten <Markus.Felten@gmx.de>
2015-04-21 22:01:13 +0200

    preparation for endpoint counterpart aut generation

--- test/flow.js
+++ test/flow.js
@@ -8,94 +8,95 @@ const flow = require('../lib/flow');
 const assert = require('assert');
 
 describe('flow declaration', function () {
-  const flows = flow.create({
-    "myFlow": {
-      "steps": {
-        "s1": {
-          "type": "copy",
-          "endpoints": {
-            "in": "stdin",
-            "out": "step:s2/in"
-          }
-        },
-        "s2": {
-          "type": "copy",
-          "endpoints": {
-            "out": "stdout"
-          }
-        }
-      }
-    }
-  });
+	const flows = flow.create({
+		"myFlow": {
+			"steps": {
+				"s1": {
+					"type": "copy",
+					"endpoints": {
+						"in": "stdin",
+						"out": "step:s2/in"
+					}
+				},
+				"s2": {
+					"type": "copy",
+					"endpoints": {
+						"out": "stdout"
+					}
+				}
+			}
+		}
+	});
 
-  const myFlow = flows.myFlow;
+	const myFlow = flows.myFlow;
 
-  it('can be initialized', function () {
-    myFlow.initialize();
-    assert(myFlow);
-  });
+	it('can be initialized', function () {
+		myFlow.initialize();
+		assert(myFlow);
+	});
 });
 
 
 describe('flow declaration', function () {
-  const flows = flow.create({
-    "myFlow": {
-      "steps": {
-        "s1": {
-          "type": "copy",
-          "config": {
-            "port": 77
-          },
-          "endpoints": {
-            "in": "stdin",
-            "out": "step:s2/in"
-          }
-        },
-        "s2": {
-          "type": "group",
-          "endpoints": {
-            "in": "steps:s2/s1/in"
-          },
-          "steps": {
-            "s1": {
-              "type": "copy"
-            },
-            "s2": {
-              "type": "copy",
-              "endpoints": {
-                "out": "file:/tmp/sample1.txt"
-              }
-            }
-          }
-        }
-      }
-    }
-  });
+	const flows = flow.create({
+		"myFlow": {
+			"steps": {
+				"s1": {
+					"type": "copy",
+					"config": {
+						"port": 77
+					},
+					"endpoints": {
+						"in": "stdin",
+						"out": "step:s2/in"
+					}
+				},
+				"s2": {
+					"type": "group",
+					"endpoints": {
+						"in": "step:s1/in",
+						"out": "step:s2/out"
+					},
+					"steps": {
+						"s1": {
+							"endpoints": {
+								"out": "step:s2/in"
+							},
+							"type": "copy"
+						},
+						"s2": {
+							"type": "copy"
+						}
+					}
+				}
+			}
+		}
+	});
 
-  const myFlow = flows.myFlow;
+	const myFlow = flows.myFlow;
 
-  it('flow name should be present', function () {
-    assert(myFlow.name === "myFlow");
-  });
+	it('flow name should be present', function () {
+		assert(myFlow.name === "myFlow");
+	});
 
-  it('steps should be present', function () {
-    assert(myFlow.steps.s1.name === "s1");
-  });
+	it('steps should be present', function () {
+		assert(myFlow.steps.s1.name === "s1");
+	});
 
-  it('steps should have a mata object', function () {
-    assert(myFlow.steps.s1.meta.name === "copy");
-  });
+	it('steps should have a mata object', function () {
+		assert(myFlow.steps.s1.meta.name === "copy");
+	});
 
-  it('steps config should be present', function () {
-    assert(myFlow.steps.s1.config.port === 77);
-  });
+	it('steps config should be present', function () {
+		assert(myFlow.steps.s1.config.port === 77);
+	});
 
-  it('endpoints should be present', function () {
-    assert(myFlow.steps.s1.endpoints.out.name === "out");
-  });
+	it('endpoints should be present', function () {
+		assert(myFlow.steps.s1.endpoints.out.name === "out");
+	});
 
-  it('substeps are present', function () {
-    assert(myFlow.steps.s2.steps.s1.name === "s1");
-  });
+	it('substeps are present', function () {
+		assert(myFlow.steps.s2.steps.s1.name === "s1");
+	});
 
 });
