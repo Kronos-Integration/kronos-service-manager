f4451d40a205b3697fdf43ebda4a5a4e00e3f628
Markus Felten <Markus.Felten@gmx.de>
2015-04-27 10:27:31 +0200

    promisified

--- test/flow_errors.js
+++ test/flow_errors.js
@@ -3,77 +3,107 @@
 
 "use strict";
 
+const chai = require('chai');
+chai.use(require("chai-as-promised"));
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+const kronos = require('../lib/manager.js');
 const flow = require('../lib/flow');
 const progressReporter = require('../lib/progressReporter');
-const assert = require('assert');
 
-describe('flow declaration endpoint missing error handling', function () {
-  let progressEntries = [];
+describe('flow declaration with errors', function () {
 
-  const myFlow = flow.create({
-    "myFlow": {
-      "steps": {
-        "s1": {
-          "type": "copy"
-        }
-      }
-    }
-  }, progressReporter.defaultProgressReporter(function (entry) {
-    progressEntries.push(entry);
-  })).myFlow;
+	function makePromise(flowDecls, progressEntries) {
+		return kronos.manager({
+			flows: flowDecls,
+			progressReporter: progressReporter.defaultProgressReporter(function (entry) {
+				progressEntries.push(entry);
+			})
+		});
+	}
 
-  //console.log(`${JSON.stringify(progressEntries)}`);
+	describe('endpoint missing error handling', function () {
+		const flowDecls = {
+			"flow1": {
+				"steps": {
+					"s1": {
+						"type": "copy"
+					}
+				}
+			}
+		};
 
-  it('progress entries should be filled with error', function () {
-    assert(progressEntries.length !== 0);
-    const pe = progressEntries[0];
-    assert(pe.severity === 'error');
-    assert(pe.properties.endpoint === 'in');
-    assert(pe.message === 'Mandatory ${endpoint} not defined');
-  });
+		it('progress entries should be filled with error', function (done) {
+			let progressEntries = [];
+			makePromise(flowDecls, progressEntries).then(function (manager) {
+				//console.log(`${JSON.stringify(progressEntries)}`);
 
-  it('error entry should have scope', function () {
-    const pe = progressEntries[0];
-    assert(pe.scope[0].name === 'flow');
-    assert(pe.scope[0].properties.flow === 'myFlow');
-    //assert(pe.scope[1].name === 'step');
-    //assert(pe.scope[1].properties.step === 's1');
-  });
-});
+				assert(progressEntries.length !== 0);
+				const pe = progressEntries[0];
+				assert(pe.severity === 'error');
+				assert(pe.properties.endpoint === 'in');
+				assert(pe.message === 'Mandatory ${endpoint} not defined');
+				done();
+			});
+		});
 
 
-describe('flow declaration step type error handling', function () {
-  let progressEntries = [];
+		it('error entry should have scope', function (done) {
+			let progressEntries = [];
 
-  let myFlow = flow.create({
-    "myFlow": {
-      "steps": {
-        "s1": {
-          "type": "copy2",
-          "endpoints": {
-            "in": "stdin",
-            "out": "stdout"
-          }
-        }
-      }
-    }
-  }, progressReporter.defaultProgressReporter(function (entry) {
-    progressEntries.push(entry);
-  })).myFlow;
+			makePromise(flowDecls, progressEntries).then(function (manager) {
+				const pe = progressEntries[0];
+				assert(pe.scope[0].name === 'flow');
+				assert(pe.scope[0].properties.flow === 'flow1');
+				//assert(pe.scope[1].name === 'step');
+				//assert(pe.scope[1].properties.step === 's1');
+				done();
+			});
+		});
 
-  it('progress entries should be filled with error', function () {
-    assert(progressEntries.length !== 0);
-    const pe = progressEntries[0];
-    assert(pe.severity === 'error');
-    assert(pe.properties.type === 'copy2');
-    assert(pe.message === 'Step ${type} implementation not found');
-  });
+	});
 
-  it('error entry should have scope', function () {
-    const pe = progressEntries[0];
-    assert(pe.scope[0].name === 'flow');
-    assert(pe.scope[0].properties.flow === 'myFlow');
-    assert(pe.scope[1].name === 'step');
-    assert(pe.scope[1].properties.step === 's1');
-  });
+	describe('step type error handling', function () {
+		const flowDecls = {
+			"myFlow": {
+				"steps": {
+					"s1": {
+						"type": "copy2",
+						"endpoints": {
+							"in": "stdin",
+							"out": "stdout"
+						}
+					}
+				}
+			}
+		};
+
+		it('progress entries should be filled with error', function (done) {
+			let progressEntries = [];
+
+			makePromise(flowDecls, progressEntries).then(function (manager) {
+				assert(progressEntries.length !== 0);
+				const pe = progressEntries[0];
+				assert(pe.severity === 'error');
+				assert(pe.properties.type === 'copy2');
+				assert(pe.message === 'Step ${type} implementation not found');
+				done();
+			});
+		});
+
+		it('error entry should have scope', function (done) {
+			let progressEntries = [];
+
+			makePromise(flowDecls, progressEntries).then(function (manager) {
+				const pe = progressEntries[0];
+				assert(pe.scope[0].name === 'flow');
+				assert(pe.scope[0].properties.flow === 'myFlow');
+				assert(pe.scope[1].name === 'step');
+				assert(pe.scope[1].properties.step === 's1');
+				done();
+			});
+		});
+	});
 });
