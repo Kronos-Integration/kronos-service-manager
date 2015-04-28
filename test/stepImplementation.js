bcc889622a1aec90f0d1c9d95f2e515cba739f4c
Markus Felten <Markus.Felten@gmx.de>
2015-04-28 21:48:01 +0200

    refactor(stepImplemenetations.js): implementation dictionary passed from outside

--- test/stepImplementation.js
+++ test/stepImplementation.js
@@ -15,39 +15,42 @@ const stepImplementation = require('../lib/stepImplementation');
 
 describe('step implementations', function () {
 
-	const buildinStepDir = path.join(__dirname, '..', 'lib', 'steps');
+  const buildinStepDir = path.join(__dirname, '..', 'lib', 'steps');
 
-	describe('register from directory', function () {
-		it('should resolve to some registered implementations', function (done) {
-			stepImplementation.registerStepsFromDirs(buildinStepDir).then(
-				function (regs) {
-					assert(regs.length > 1);
-					done();
-				});
-		});
-		it('should have registered copy step implementation', function (done) {
-			stepImplementation.registerStepsFromDirs(buildinStepDir).then(
-				function () {
-					const si = stepImplementation.implementations.copy;
-					assert(si.name === "copy");
-					assert(si.endpoints.in.name === "in");
+  describe('register from directory', function () {
+    it('should resolve to some registered implementations', function (done) {
+      let all = {};
+      stepImplementation.registerStepsFromDirs(all, buildinStepDir).then(
+        function (regs) {
+          assert(regs.length > 1);
+          done();
+        });
+    });
+    it('should have registered copy step implementation', function (done) {
+      let all = {};
 
-					assert(si.endpoints.in.mandatory);
-					assert(si.endpoints.in.uti === 'public.data');
-					assert(si.endpoints.in.direction === 'in');
+      stepImplementation.registerStepsFromDirs(all, buildinStepDir).then(
+        function () {
+          const si = all.copy;
+          assert(si.name === "copy");
+          assert(si.endpoints.in.name === "in");
 
-					assert(si.endpoints.out.name === "out");
-					assert(si.endpoints.out.direction === 'out');
+          assert(si.endpoints.in.mandatory);
+          assert(si.endpoints.in.uti === 'public.data');
+          assert(si.endpoints.in.direction === 'in');
 
-					assert(si.endpoints.out.uti === 'public.data');
-					assert(si.endpoints.out.mandatory);
-					assert(si.endpoints.out.contentInfo);
+          assert(si.endpoints.out.name === "out");
+          assert(si.endpoints.out.direction === 'out');
 
-					assert(si.endpoints.log.name === "log");
-					assert(si.endpoints.log.mandatory);
-					assert(si.endpoints.log.direction === 'out');
-					done();
-				});
-		});
-	});
+          assert(si.endpoints.out.uti === 'public.data');
+          assert(si.endpoints.out.mandatory);
+          assert(si.endpoints.out.contentInfo);
+
+          assert(si.endpoints.log.name === "log");
+          assert(si.endpoints.log.mandatory);
+          assert(si.endpoints.log.direction === 'out');
+          done();
+        });
+    });
+  });
 });
