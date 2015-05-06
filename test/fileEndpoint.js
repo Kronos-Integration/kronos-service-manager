8d48b736a14a55f521f93c37f2302581453ffab7
Markus Felten <Markus.Felten@gmx.de>
2015-05-07 00:03:52 +0200

    fixed(test): use createEndpoint()

--- test/fileEndpoint.js
+++ test/fileEndpoint.js
@@ -11,28 +11,24 @@ const endpointImpl = require('../lib/endpointImplementation');
 
 describe('file endpoint implementation', function () {
 
-  const fileImpl = endpointImpl.implementations.file;
+	const fileImpl = endpointImpl.implementations.file;
 
-  const myEndpoint = Object.create(endpointImpl.defaultEndpoint, {
-    value: {
-      value: "file:" + path.join(__dirname, 'fixtures', 'file1.txt')
-    },
-    direction: {
-      value: 'in'
-    }
-  });
+	const myEndpoint = endpointImpl.createEndpoint('e1', {
+		target: "file:" + path.join(__dirname, 'fixtures', 'file1.txt'),
+		direction: 'in'
+	});
 
-  const in1 = fileImpl.implementation(myEndpoint)();
+	const in1 = fileImpl.implementation(myEndpoint)();
 
-  //console.log(`in1: ${in1}`);
+	//console.log(`in1: ${in1}`);
 
-  for (let request in in1) {
-    console.log(`request: ${request}`);
-  }
+	for (let request in in1) {
+		console.log(`request: ${request}`);
+	}
 
-  /*
-    let request = in1.next();
-    assert(request.stream !== undefined);
-  */
+	/*
+	  let request = in1.next();
+	  assert(request.stream !== undefined);
+	*/
 
 });
