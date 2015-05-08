de3b0f021318ba9fe185cc17b2f3abce12ad9a0e
Markus Felten <Markus.Felten@gmx.de>
2015-05-08 09:48:50 +0200

    object/direct target/implementation tests

--- test/endpoints.js
+++ test/endpoints.js
@@ -13,50 +13,67 @@ const endpointImpl = require('../lib/endpointImplementation');
 
 describe('endpoint definition', function () {
 
-	describe('with implementation function', function () {
-		it('implementation present', function () {
-			function myImplementation() {}
-			const endpoint = endpointImpl.createEndpoint('e1', {
-				implementation: myImplementation
-			});
-			assert(endpoint.implementation === myImplementation);
-		});
-	});
+  describe('with implementation function', function () {
+    function myImplementation() {}
 
-	describe('with target', function () {
-		it('target present', function () {
-			const endpoint = endpointImpl.createEndpoint('e1', {
-				target: "myTarget"
-			});
-			assert(endpoint.target === 'myTarget');
-		});
-	});
+    describe('as object', function () {
+      it('implementation present', function () {
+        const endpoint = endpointImpl.createEndpoint('e1', {
+          implementation: myImplementation
+        });
+        assert(endpoint.implementation === myImplementation);
+      });
+    });
+    describe('as direct function', function () {
+      it('implementation present', function () {
+        const endpoint = endpointImpl.createEndpoint('e1', myImplementation);
+        assert(endpoint.implementation === myImplementation);
+      });
+    });
+  });
 
-	describe('should have correct direction', function () {
-		it('for in', function () {
-			const endpoint = endpointImpl.createEndpoint('e1', {
-				direction: 'in'
-			});
-			assert(endpoint.direction === 'in');
-			assert(endpoint.isIn, "isIn when in");
-		});
+  describe('with target', function () {
+    describe('as object', function () {
+      it('target present', function () {
+        const endpoint = endpointImpl.createEndpoint('e1', {
+          target: "myTarget"
+        });
+        assert(endpoint.target === 'myTarget');
+      });
+    });
+    describe('as direct string', function () {
+      it('target present', function () {
+        const endpoint = endpointImpl.createEndpoint('e1', "myTarget");
+        assert(endpoint.target === 'myTarget');
+      });
+    });
+  });
 
-		it('for out', function () {
-			const endpoint = endpointImpl.createEndpoint('e1', {
-				direction: 'out'
-			});
-			assert(endpoint.direction === 'out');
-			assert(endpoint.isOut, "isOut when out");
-		});
+  describe('should have correct direction', function () {
+    it('for in', function () {
+      const endpoint = endpointImpl.createEndpoint('e1', {
+        direction: 'in'
+      });
+      assert(endpoint.direction === 'in');
+      assert(endpoint.isIn, "isIn when in");
+    });
 
-		it('for inout', function () {
-			const endpoint = endpointImpl.createEndpoint('e1', {
-				direction: 'inout'
-			});
-			assert(endpoint.direction === 'inout');
-			assert(endpoint.isOut, "isOut when inout");
-			assert(endpoint.isIn, "isIn when inout");
-		});
-	});
+    it('for out', function () {
+      const endpoint = endpointImpl.createEndpoint('e1', {
+        direction: 'out'
+      });
+      assert(endpoint.direction === 'out');
+      assert(endpoint.isOut, "isOut when out");
+    });
+
+    it('for inout', function () {
+      const endpoint = endpointImpl.createEndpoint('e1', {
+        direction: 'inout'
+      });
+      assert(endpoint.direction === 'inout');
+      assert(endpoint.isOut, "isOut when inout");
+      assert(endpoint.isIn, "isIn when inout");
+    });
+  });
 
 });
