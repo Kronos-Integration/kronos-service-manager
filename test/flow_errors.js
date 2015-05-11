f557bf21742b706faf62f1c8bf20f9ff8c8312dc
Torsten Link <torsten.link@db.com>
2015-05-12 00:01:55 +0200

    add the schema check to the manager.

--- test/flow_errors.js
+++ test/flow_errors.js
@@ -15,95 +15,97 @@ const progressReporter = require('../lib/progressReporter');
 
 describe('flow declaration with errors', function () {
 
-	function makePromise(flowDecls, progressEntries) {
-		return kronos.manager({
-			flows: flowDecls,
-			progressReporter: progressReporter.defaultProgressReporter(function (entry) {
-				progressEntries.push(entry);
-			})
-		});
-	}
+  function makePromise(flowDecls, progressEntries) {
+    return kronos.manager({
+      flows: flowDecls,
+      progressReporter: progressReporter.defaultProgressReporter(function (entry) {
+        progressEntries.push(entry);
+      })
+    });
+  }
 
-	describe('endpoint missing error handling', function () {
-		const flowDecls = {
-			"flow1": {
-				"steps": {
-					"s1": {
-						"type": "copy"
-					}
-				}
-			}
-		};
+  describe('endpoint missing error handling', function () {
+    const flowDecls = {
+      "flow1": {
+        "description": "Test",
+        "steps": {
+          "s1": {
+            "type": "copy"
+          }
+        }
+      }
+    };
 
-		it('progress entries should be filled with error', function (done) {
-			let progressEntries = [];
-			makePromise(flowDecls, progressEntries).then(function (manager) {
-				//console.log(`${JSON.stringify(progressEntries)}`);
+    it('progress entries should be filled with error', function (done) {
+      let progressEntries = [];
+      makePromise(flowDecls, progressEntries).then(function (manager) {
+        //console.log(`${JSON.stringify(progressEntries)}`);
 
-				assert(progressEntries.length !== 0);
-				const pe = progressEntries[0];
-				assert(pe.severity === 'error');
-				assert(pe.properties.endpoint === 'in');
-				assert(pe.message === 'Mandatory ${endpoint} not defined');
-				done();
-			});
-		});
+        assert(progressEntries.length !== 0);
+        const pe = progressEntries[0];
+        assert(pe.severity === 'error');
+        assert(pe.properties.endpoint === 'in');
+        assert(pe.message === 'Mandatory ${endpoint} not defined');
+        done();
+      });
+    });
 
 
-		it('error entry should have scope', function (done) {
-			let progressEntries = [];
+    it('error entry should have scope', function (done) {
+      let progressEntries = [];
 
-			makePromise(flowDecls, progressEntries).then(function (manager) {
-				const pe = progressEntries[0];
-				assert(pe.scope[0].name === 'flow');
-				assert(pe.scope[0].properties.flow === 'flow1');
-				//assert(pe.scope[1].name === 'step');
-				//assert(pe.scope[1].properties.step === 's1');
-				done();
-			});
-		});
+      makePromise(flowDecls, progressEntries).then(function (manager) {
+        const pe = progressEntries[0];
+        assert(pe.scope[0].name === 'flow');
+        assert(pe.scope[0].properties.flow === 'flow1');
+        //assert(pe.scope[1].name === 'step');
+        //assert(pe.scope[1].properties.step === 's1');
+        done();
+      });
+    });
 
-	});
+  });
 
-	describe('step type error handling', function () {
-		const flowDecls = {
-			"myFlow": {
-				"steps": {
-					"s1": {
-						"type": "copy2",
-						"endpoints": {
-							"in": "stdin",
-							"out": "stdout"
-						}
-					}
-				}
-			}
-		};
+  describe('step type error handling', function () {
+    const flowDecls = {
+      "myFlow": {
+        "description": "Test",
+        "steps": {
+          "s1": {
+            "type": "copy2",
+            "endpoints": {
+              "in": "stdin",
+              "out": "stdout"
+            }
+          }
+        }
+      }
+    };
 
-		it('progress entries should be filled with error', function (done) {
-			let progressEntries = [];
+    it('progress entries should be filled with error', function (done) {
+      let progressEntries = [];
 
-			makePromise(flowDecls, progressEntries).then(function (manager) {
-				assert(progressEntries.length !== 0);
-				const pe = progressEntries[0];
-				assert(pe.severity === 'error');
-				assert(pe.properties.type === 'copy2');
-				assert(pe.message === 'Step ${type} implementation not found');
-				done();
-			});
-		});
+      makePromise(flowDecls, progressEntries).then(function (manager) {
+        assert(progressEntries.length !== 0);
+        const pe = progressEntries[0];
+        assert(pe.severity === 'error');
+        assert(pe.properties.type === 'copy2');
+        assert(pe.message === 'Step ${type} implementation not found');
+        done();
+      });
+    });
 
-		it('error entry should have scope', function (done) {
-			let progressEntries = [];
+    it('error entry should have scope', function (done) {
+      let progressEntries = [];
 
-			makePromise(flowDecls, progressEntries).then(function (manager) {
-				const pe = progressEntries[0];
-				assert(pe.scope[0].name === 'flow');
-				assert(pe.scope[0].properties.flow === 'myFlow');
-				assert(pe.scope[1].name === 'step');
-				assert(pe.scope[1].properties.step === 's1');
-				done();
-			});
-		});
-	});
+      makePromise(flowDecls, progressEntries).then(function (manager) {
+        const pe = progressEntries[0];
+        assert(pe.scope[0].name === 'flow');
+        assert(pe.scope[0].properties.flow === 'myFlow');
+        assert(pe.scope[1].name === 'step');
+        assert(pe.scope[1].properties.step === 's1');
+        done();
+      });
+    });
+  });
 });
--- test/stdin_to_stdout.js
+++ test/stdin_to_stdout.js
@@ -12,40 +12,41 @@ iojs test/stdin_to_stdout.js <test/stdin_to_stdout.js
 const kronos = require('../lib/manager.js');
 
 const floDecls = {
-	"flow1": {
-		"steps": {
-			"s1": {
-				"type": "copy",
-				"endpoints": {
-					"in": "stdin",
-					"out": "step:s2/in"
-				}
-			},
-			"s2": {
-				"type": "copy",
-				"endpoints": {
-					"out": "step:s3/in"
-				}
-			},
-			"s3": {
-				"type": "copy",
-				"endpoints": {
-					"out": "step:s4/in"
-				}
-			},
-			"s4": {
-				"type": "copy",
-				"endpoints": {
-					"out": "stdout"
-				}
-			}
-		}
-	}
+  "flow1": {
+    "description": "Test",
+    "steps": {
+      "s1": {
+        "type": "copy",
+        "endpoints": {
+          "in": "stdin",
+          "out": "step:s2/in"
+        }
+      },
+      "s2": {
+        "type": "copy",
+        "endpoints": {
+          "out": "step:s3/in"
+        }
+      },
+      "s3": {
+        "type": "copy",
+        "endpoints": {
+          "out": "step:s4/in"
+        }
+      },
+      "s4": {
+        "type": "copy",
+        "endpoints": {
+          "out": "stdout"
+        }
+      }
+    }
+  }
 };
 
 kronos.manager({
-	flows: floDecls
+  flows: floDecls
 }).then(function (manager) {
-	manager.intializeFlow('flow1');
-	//const flow = manager.flowDefinitions.flow1.initialize(manager);
+  manager.intializeFlow('flow1');
+  //const flow = manager.flowDefinitions.flow1.initialize(manager);
 });
