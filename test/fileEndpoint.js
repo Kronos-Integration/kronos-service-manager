753ab4b542663d34e0255eb71406c6a5061b66ff
Markus Felten <Markus.Felten@gmx.de>
2015-05-13 11:49:07 +0200

    fixed(file endpoint): stream content is present

--- test/fileEndpoint.js
+++ test/fileEndpoint.js
@@ -9,36 +9,43 @@ const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
+const streamEqual = require('stream-equal');
 
 const endpointImpl = require('../lib/endpointImplementation');
 
 describe('file endpoint implementation', function () {
 
-  const fileImpl = endpointImpl.implementations.file;
-  const fileName = path.join(__dirname, 'fixtures', 'file1.txt');
+	const fileImpl = endpointImpl.implementations.file;
+	const fileName = path.join(__dirname, 'fixtures', 'file1.txt');
 
-  const endpoint = endpointImpl.createEndpoint('e1', {
-    target: "file:" + fileName,
-    direction: 'in'
-  }, fileImpl);
+	const endpoint = endpointImpl.createEndpoint('e1', {
+		target: "file:" + fileName,
+		direction: 'in'
+	}, fileImpl);
 
-  it("should have an implementation", function () {
-    assert(fileImpl !== undefined);
-    assert(endpoint.implementation === fileImpl.implementation, "file endpoint implementation");
-  });
+	it("should have an implementation", function () {
+		assert(fileImpl !== undefined);
+		assert(endpoint.implementation === fileImpl.implementation, "file endpoint implementation");
+	});
 
-  let g = endpoint.initialize({}, {
-    name: "myStep"
-  });
+	let g = endpoint.initialize({}, {
+		name: "myStep"
+	})();
 
-  it("should have a request", function () {
-    let number = 0;
+	it("should have a request", function (done) {
+		let number = 0;
 
-    for (let request in g) {
-      number++;
-      assert(request.info.name === fileName + "ss");
-      console.log(`request: ${request}`);
-    }
-    assert(number === 1, "exactly one request");
-  });
+		function equalizer(err, equal) {
+			assert(equal, "stream is equal to file content");
+			done();
+		}
+
+		for (let request of g) {
+			number++;
+			assert(request.info.name === fileName, "file name is " + fileName);
+
+			streamEqual(request.stream, fs.createReadStream(fileName), equalizer);
+		}
+		assert(number === 1, "exactly one request");
+	});
 });
