1e6074de4818f390958fcd4d080d8bcad834e96e
Markus Felten <Markus.Felten@gmx.de>
2015-05-18 23:24:15 +0200

    simple api test added

--- test/stdioEndpoint.js
+++ test/stdioEndpoint.js
@@ -12,18 +12,33 @@ const should = chai.should();
 
 const endpointImpl = require('../lib/endpointImplementation');
 
+describe('stdin endpoint', function () {
+  const endpoint = endpointImpl.createEndpoint('e1', {
+    target: "stdin"
+  }, endpointImpl.implementations.stdin);
+
+  let in1 = endpoint.initialize({}, {
+    name: "myStep"
+  });
+
+  it("should produce a request", function () {
+    let gen = in1.next();
+    let request = gen.value;
+    assert(request.info.name === 'stdin');
+    assert(request.stream !== undefined);
+  });
+});
 
 describe('stdout endpoint', function () {
   const endpoint = endpointImpl.createEndpoint('e1', {
-    target: "stdout",
-    direction: 'out'
+    target: "stdout"
   }, endpointImpl.implementations.stdout);
 
   let out = endpoint.initialize({}, {
     name: "myStep"
-  })();
+  });
 
-  it("should consume a request", function (done) {
+  it("should consume a request", function () {
     const fileName = path.join(__dirname, 'fixtures', 'file1.txt');
 
     out.next({
