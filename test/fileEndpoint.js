070b57856403ae8c370b94813bd4200a3eadfa30
Markus Felten <Markus.Felten@gmx.de>
2015-05-28 07:53:10 +0200

    file endpoint out not working correctly

--- test/fileEndpoint.js
+++ test/fileEndpoint.js
@@ -10,7 +10,7 @@ const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
 const streamEqual = require('stream-equal');
-
+const tmp = require('tmp');
 const endpointImpl = require('../lib/endpointImplementation');
 
 const fileImpl = endpointImpl.implementations.file;
@@ -27,9 +27,7 @@ describe('in file endpoint', function () {
 		assert(endpoint.implementation === fileImpl.implementation, "file endpoint implementation");
 	});
 
-	let requests = endpoint.initialize({}, {
-		name: "myStep"
-	});
+	let requests = endpoint.initialize();
 
 	it("should have a request", function (done) {
 		let number = 0;
@@ -50,26 +48,36 @@ describe('in file endpoint', function () {
 });
 
 describe('out file endpoint', function () {
-
-	const outFileName = path.join(__dirname, 'fixtures', 'file2.txt');
-
-	const endpoint = endpointImpl.createEndpoint('e1', {
-		target: "file:" + outFileName,
-		direction: 'out'
-	}, fileImpl);
-
-	let out = endpoint.initialize({}, {
-		name: "myStep"
-	});
-
 	it("should consume a request", function (done) {
-		out.next({
-			info: {
-				name: "aName"
-			},
-			stream: fs.createReadStream(inFileName)
-		});
 
-		done();
+		tmp.file(function (err, outFileName) {
+			const endpoint = endpointImpl.createEndpoint('e1', {
+				target: "file:" + outFileName,
+				direction: 'out(push)'
+			}, fileImpl);
+
+			let out = endpoint.initialize();
+
+			out.next({
+				info: {
+					name: "aName"
+				},
+				stream: fs.createReadStream(inFileName)
+			});
+
+			setTimeout(function () {
+				fs.stat(outFileName, function (err, stat) {
+					console.log(`${err} size: ${stat.size}`);
+				});
+
+				function equalizer(err, equal) {
+					console.log(`equal: ${equal}`);
+					assert(equal, "stream is equal to file content");
+					done();
+				}
+
+				streamEqual(fs.createReadStream(outFileName), fs.createReadStream(inFileName), equalizer);
+			}, 100);
+		});
 	});
 });
