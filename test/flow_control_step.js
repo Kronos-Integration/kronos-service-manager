beaf685616268b23456b8eda1dd1e2324987062c
Markus Felten <Markus.Felten@gmx.de>
2015-06-08 17:26:41 +0200

    WIP

---
+++ test/flow_control_step.js
@@ -0,0 +1,42 @@
+/* global describe, it, xit */
+/* jslint node: true, esnext: true */
+
+"use strict";
+
+const chai = require('chai');
+chai.use(require("chai-as-promised"));
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+const path = require('path');
+const fs = require('fs');
+
+const kronos = require('../lib/manager.js');
+
+describe('service manager', function () {
+  const flowDecl = {
+    "flow1": {
+      "steps": {
+        "s1": {
+          "type": "flow_control",
+          "endpoints": {
+            "in": function* () {
+                yield { info : { name : "myFlow" }, stream: fs.createReadStream(path.join(__dirname,'fixtures','sample.flow'))}
+            }
+          }
+        }
+      }
+    }
+  };
+
+  describe('flow_control', function () {
+    it('excec within flow1', function (done) {
+      kronos.manager().then(function (myManager) {
+        myManager.declareFlows(flowDecl);
+        myManager.intializeFlow('flow1');
+        done();
+      });
+    });
+  });
+});
