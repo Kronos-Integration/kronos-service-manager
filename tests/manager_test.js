01a23dfbb5ef563272c6f35c2e3473db34aef811
Markus Felten <Markus.Felten@gmx.de>
2015-12-21 15:34:01 +0100

    test: separate services test into its own file services_test.js

--- tests/manager_test.js
+++ tests/manager_test.js
@@ -44,67 +44,6 @@ describe('service manager', function () {
     });
   });
 
-  describe('services', function () {
-    it('simple definition', function (done) {
-      kronos.manager().then(function (manager) {
-          try {
-            const value1 = 4711;
-
-            assert.equal(manager.serviceGet('service1'), undefined);
-            assert.equal(manager.serviceGet('service1', serviceName => {
-              return {
-                key1: value1
-              };
-            }).key1, value1);
-            assert.equal(manager.serviceGet('service1').key1, value1);
-            assert.equal(manager.serviceGet('service1').state, 'stopped');
-            done();
-          } catch (e) {
-            done(e);
-          }
-        },
-        function () {
-          done("Manager not created");
-        });
-    });
-    it('derived definition', function (done) {
-      kronos.manager().then(function (manager) {
-          try {
-            const returnedService = manager.serviceRegister('abstract', {
-              _start() {
-                  return Promise.resolve(this);
-                },
-                abstractKey: 'abstractValue'
-            });
-
-            const abstract = manager.serviceGet('abstract');
-            assert.equal(abstract, returnedService);
-
-            assert.equal(abstract.name, 'abstract');
-            assert.equal(abstract.abstractKey, 'abstractValue');
-
-            manager.serviceRegister('derived', {
-              name: 'abstract',
-              derivedKey: 'derivedValue'
-            });
-
-            const derived = manager.serviceGet('derived');
-            assert.equal(derived.name, 'derived');
-            assert.equal(derived.abstractKey, 'abstractValue');
-
-            assert.equal(derived.derivedKey, 'derivedValue');
-
-            done();
-          } catch (e) {
-            done(e);
-          }
-        },
-        function () {
-          done("Manager not created");
-        });
-    });
-  });
-
   describe('uti definitions', function () {
     it('should be present', function (done) {
       kronos.manager().then(function (manager) {
