3c176af4342f7f563bd3978b5a6b241df43b6162
Markus Felten <Markus.Felten@gmx.de>
2017-12-27 23:10:59 +0100

    refactor: make use of some more step / flow mixins

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,14 @@ import pkg from './package.json';
 
 export default {
   plugins: [],
-  external: ['kronos-endpoint', 'kronos-service', 'model-attributes', 'registry-mixin'],
+  external: [
+    'kronos-endpoint',
+    'kronos-service',
+    'model-attributes',
+    'kronos-step',
+    'kronos-flow',
+    'kronos-service'
+  ],
   input: pkg.module,
 
   output: {
--- src/service-manager.js
+++ src/service-manager.js
@@ -1,12 +1,19 @@
-import { ServiceProviderMixin, Service } from 'kronos-service';
+import {
+  ServiceProviderMixin,
+  InterceptorProviderMixin,
+  Service
+} from 'kronos-service';
+import { StepProviderMixin } from 'kronos-step';
+import { FlowProviderMixin } from 'kronos-flow';
 import { createAttributes, mergeAttributes } from 'model-attributes';
 import { SendEndpoint } from 'kronos-endpoint';
-import { defineRegistryProperties } from 'registry-mixin';
 
-import FlowSupportMixin from './FlowSupportMixin';
+import FlowSupportMixin from './flow-support-mixin';
 
 export class ServiceManager extends FlowSupportMixin(
-  ServiceProviderMixin(Service)
+  ServiceProviderMixin(
+    FlowProviderMixin(StepProviderMixin(InterceptorProviderMixin(Service)))
+  )
 ) {
   /**
    * @return {string} 'kronos'
@@ -35,34 +42,6 @@ export class ServiceManager extends FlowSupportMixin(
     defineRegistryProperties(this, 'interceptor', {
       withCreateInstance: true,
       factoryType: 'new'
-    });
-
-    /*
-     * createStepInstance(type,...args);
-     * createStepInstanceFromConfig({ type: "type name" },...args);
-     *   calls: registeredStep.createInstance( config, ...args)
-     */
-    defineRegistryProperties(this, 'step', {
-      withCreateInstance: true,
-      withEvents: true,
-      factoryType: 'object',
-      factoryMethod: 'createInstance'
-      // todo pass 'registry' as 2nd. argument
-    });
-
-    defineRegistryProperties(this, 'flow', {
-      withEvents: true,
-      hasBeenRegistered: flow =>
-        flow.autostart ? flow.start() : Promise.resolve(),
-
-      /**
-       * Deletes a flow from the stored flow definitions. If the flow
-       * is currently running, it will be stopped first. After it
-       * is stopped, it will be deleted.
-       * @return {Promise} returns a promise that is fullfilled when the flow is removed
-       *         or one that rejects if there is no flow for the given flowName
-       */
-      willBeUnregistered: flow => flow.stop().then(flow.remove())
     });
 
     const manager = this;
